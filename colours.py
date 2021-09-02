93c7dd74bd5990f3e5b2782221d5f1e3e7661c8d
Cameron Clarry <cameronclarry@gmail.com>
2021-09-01 23:29:58 -0400

    Move generated colourschemes to subdirectory

---
+++ colours.py
@@ -0,0 +1,61 @@
+import argparse
+import subprocess
+import os
+import re
+
+# Query xrdb
+result = subprocess.run(['xrdb', '-query'], stdout=subprocess.PIPE)
+lines = result.stdout.decode('utf-8').split('\n')
+
+# Read the lines to get the settings
+settings_re = re.compile(r'(.*)\.(.*):\s*([^\s]+)')
+settings = {}
+for line in lines:
+    match = re.match(settings_re, line)
+    if match:
+        program = match.group(1)
+        field = match.group(2)
+        value = match.group(3)
+        if not program in settings:
+            settings[program] = {}
+
+        settings[program][field] = value
+
+# Read the list of templates to fill out
+programs = {}
+try:
+    with open('templates.txt', 'r') as f:
+        for line in f.readlines():
+            parts = line.split(':')
+            programs[parts[0]] = parts[1].strip()
+except FileNotFoundError:
+    with open('templates.txt', 'w'):
+        print('No template list found, made an empty file')
+
+# For each program, read its template file and then save it
+settings_re = re.compile(r'\${(.*)}')
+for program in programs:
+    config_path = programs[program]
+    template_path = '%s.template'%config_path
+    if not program in settings:
+        print('No settings found for %s, skipping'%program)
+        continue
+
+    if not os.path.isfile(template_path):
+        print('Could not find the template for %s with path %s'%(program, template_path))
+
+    lines_out = []
+    with open(template_path, 'r') as tfile:
+        lines_in = tfile.readlines()
+        for line in lines_in:
+            match = re.search(settings_re, line)
+            if match:
+                field = match.group(1)
+                if field in settings[program]:
+                    line = re.sub(settings_re, settings[program][field], line)
+
+            lines_out.append(line)
+
+    with open(config_path, 'w') as cfile:
+        cfile.writelines(lines_out)
+
---
+++ setscheme.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+# 1: image path
+# 2: colourscheme name
+# First, check if the image file exists. If not, exit
+if [[ ! -a "$1" ]]; then
+	echo "Could not locate file $1"
+	exit 1
+fi
+
+# Check if the colourscheme file is valid
+if [[ -z "$2" ]]; then
+	echo "Invalid colourscheme name given"
+	exit 1
+fi
+
+# If no colouscheme directory exists, create it
+if [[ ! -d "$HOME/.config/colours/colourschemes" ]]; then
+	mkdir $HOME/.config/colours/colourschemes
+	echo "Made colourscheme directory"
+fi
+
+# Then check if the colourscheme file exists. If not, generate it
+
+if [[ ! -a "$HOME/.config/colours/colourschemes/$2" ]]; then
+	echo "Generating colourscheme"
+	python $HOME/.config/colours/colourscheme.py "$1" -s 10 > "$HOME/.config/colours/colourschemes/$2"
+fi
+
+# Then do a symbolic link from that colourscheme to .config/colours/current
+if [[ -a "$HOME/.config/colours/current" ]]; then
+	rm "$HOME/.config/colours/current"
+	echo "Removed existing colourscheme symlink"
+fi
+
+ln -s "$HOME/.config/colours/colourschemes/$2" "$HOME/.config/colours/current"
+
+# xrdb -merge
+
+xrdb -merge "$HOME/.Xresources"
+
+# Run the update script
+
+python "$HOME/.config/colours/colours.py"
+
+# Set the desktop background
+
+if [[ -L "$HOME/.config/colours/currentbg" ]]; then
+	rm "$HOME/.config/colours/currentbg"
+	echo "Removed existing background symlink"
+fi
+
+ln -s "$1" "$HOME/.config/colours/currentbg"
+feh --bg-fill "$(readlink "$HOME/.config/colours/currentbg")"
