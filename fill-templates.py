488b252d1377339763db64efcc108eebf992e94d
Cameron Clarry <cameronclarry@gmail.com>
2022-10-21 16:30:55 -0400

    Allow running fill-templates.py from anywhere, improve comments/output

--- fill-templates.py
+++ fill-templates.py
@@ -1,3 +1,6 @@
+# Some programs do not allow access to X resources colours from their config files.
+# For these cases, this script fills a template config with the correct colours.
+# The config files are specified in templates.txt in the root directory of the repository.
 import subprocess
 import os
 import re
@@ -6,7 +9,7 @@ import re
 result = subprocess.run(['xrdb', '-query'], stdout=subprocess.PIPE)
 lines = result.stdout.decode('utf-8').split('\n')
 
-# Read the lines to get the settings
+# Read the output to get the settings
 settings_re = re.compile(r'(.*)\.(.*):\s*([^\s]+)')
 settings = {}
 for line in lines:
@@ -22,7 +25,7 @@ for line in lines:
 
 # Read the list of templates to fill out
 programs = {}
-template_list_path = os.path.expanduser('~/.config/colours/templates.txt')
+template_list_path = os.path.join(os.path.dirname(__file__), 'templates.txt')
 try:
     with open(template_list_path, 'r') as f:
         for line in f.readlines():
@@ -32,7 +35,7 @@ except FileNotFoundError:
     with open(template_list_path, 'w'):
         print('No template list found, made an empty file')
 
-# For each program, read its template file and then save it
+# Make the substitutions in each program's template file, then save the config file
 settings_re = re.compile(r'\${(.*)}')
 for program in programs:
     config_path = programs[program]
@@ -42,7 +45,8 @@ for program in programs:
         continue
 
     if not os.path.isfile(template_path):
-        print('Could not find the template for %s with path %s'%(program, template_path))
+        print('Could not find the template for %s with path %s, skipping'%(program, template_path))
+        continue
 
     lines_out = []
     with open(template_path, 'r') as tfile:
